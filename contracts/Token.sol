04f8a1707a3742f556609da589378920e969f459
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-02-29 01:07:19 -0600

    7. Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,7 +13,6 @@ contract Token {
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
@@ -25,14 +24,19 @@ contract Token {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+    	require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint256 _value) public returns(bool success) {
@@ -44,4 +48,36 @@ contract Token {
         return true;
     }
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+    	require(_value <= balanceOf[_from]);
+    	require(_value <= allowance[_from][msg.sender]);
+
+    	allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+    	_transfer(_from, _to, _value);
+
+    	return true;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
